5dd9a617ab56dc2fe5d5a263f2665df60611b578
Maarja Laadi <maarja.laadi@voco.ee>
2024-01-25 09:42:34 +0200

    Added expenseData

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
-  return (
-    <div className="App">
-    <ExpenseItem />
-    </div>
-  );
-}
+  const expenses = [ 
+  { 
+    date: new Date(2023, 9, 6)
+    title: 'New book'
+    price: 30.99
+  },
+ {
+  date: new Date(2023, 9, 6)
+  title: 'New jeans'
+  price: 99.99
+ } 
+]
+return (
+  <div className="App">
+  <ExpenseItem 
+expenseData={expenses[0]}
+  />
+    <ExpenseItem 
+expenseData={expenses[1]}
+  />
+  </div>
+);}
 
 export default App;

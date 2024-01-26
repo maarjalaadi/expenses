99b601ab7bcb0c9c80d3d52b39a9bfa03fd29be2
Maarja Laadi <maarja.laadi@voco.ee>
2024-01-26 15:17:45 +0200

    Added click me button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,25 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
+import{ useState } from 'react' 
 
-function ExpenseItem(props){
+const ExpenseItem = (props) => {
+    const[title, setTitle] = useState(props.expenseData.title) 
 
-    return (
-        <div className="expense-item">
-        <ExpenseDate date={props.expenseData.date} />
+    const clickHandler = () =>{
+        console.log('Clicked!')
+        setTitle(`Updated by click ${title}`)
+        console.log(title)
+    } 
+    return(
+        <Card className="expense-item">
+            <ExpenseDate date={props.expenseData.date} />
             <div className="expense-item__description">
-            <h2>{props.expenseData.title}</h2>
-            <div className="expense-item__price">{props.expenseData.price}</div>
+                <h2>{props.expenseData.title}</h2>
+                <div className="expense-item__price">{props.expenseData.price}</div>
         </div>
-    </div>
+        <button onClick={clickHandler}>Click Me</button>
+        </Card>
     )
 }   
 
